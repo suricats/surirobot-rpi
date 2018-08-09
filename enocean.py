292c3276dfa1dc1b51091e3d30012dea31ddd949
Alain BERRIER <alain.berrier@outlook.com>
2018-08-09 12:02:11 +0200

    Initial commit

---
+++ enocean.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from enocean.consolelogger import init_logging
+import enocean.utils
+from enocean.communicators.serialcommunicator import SerialCommunicator
+from enocean.protocol.packet import RadioPacket
+from enocean.protocol.constants import PACKET, RORG
+import sys
+import traceback
+import sys
+print(sys.version_info)
+try:
+    import queue
+except ImportError:
+    import Queue as queue
+
+
+def assemble_radio_packet(transmitter_id):
+    return RadioPacket.create(rorg=RORG.BS4, rorg_func=0x20, rorg_type=0x01,
+                              sender=transmitter_id,
+                              CV=50,
+                              TMP=21.5,
+                              ES='true')
+
+
+init_logging()
+communicator = SerialCommunicator(port='/dev/ttyUSB0')
+communicator.start()
+print('The Base ID of your module is %s.' % enocean.utils.to_hex_string(communicator.base_id))
+
+if communicator.base_id is not None:
+    print('Sending example package.')
+    communicator.send(assemble_radio_packet(communicator.base_id))
+
+# endless loop receiving radio packets
+while communicator.is_alive():
+    try:
+        # Loop to empty the queue...
+        packet = communicator.receive.get(block=True, timeout=1)
+
+        if packet.packet_type == PACKET.RADIO and packet.rorg == RORG.BS4:
+            print('WESH')
+            # parse packet with given FUNC and TYPE
+            for k in packet.parse_eep(0x02, 0x05):
+                if k == 'TMP':
+                    print('{}: {:2f}{}'.format(k, float(packet.parsed[k]['value']), packet.parsed[k]['unit']))
+                else:
+                    print('{}: {}'.format(k, packet.parsed[k]))
+        if packet.packet_type == PACKET.RADIO and packet.rorg == RORG.BS1:
+            print('WALLAH')
+            # alternatively you can select FUNC and TYPE explicitely
+            packet.select_eep(0x00, 0x01)
+            # parse it
+            packet.parse_eep()
+            for k in packet.parsed:
+                print('SUUUUUUUU')
+                print('%s: %s' % (k, packet.parsed[k]))
+        if packet.packet_type == PACKET.RADIO and packet.rorg == RORG.RPS:
+            for k in packet.parse_eep(0x02, 0x02):
+                print('SLT')
+                print('%s: %s' % (k, packet.parsed[k]))
+    except queue.Empty:
+        continue
+    except KeyboardInterrupt:
+        break
+    except Exception:
+        traceback.print_exc(file=sys.stdout)
+        break
+
+if communicator.is_alive():
+    communicator.stop()
---
+++ temp.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# SCRIPT FOR DHT11 SENSOR
+"""
+PIN CONFIGURATION ON RPI 3
+VCC | X | GND | X | ....... | USB   |
+X   | X |  X  | SIGNAL | .. | PORTS |
+"""
+from dotenv import load_dotenv, find_dotenv
+import os
+from time import sleep
+import requests
+import json
+
+# Load .env
+load_dotenv(find_dotenv())
+
+try:
+    import Adafruit_DHT
+except ImportError:
+    print('Adafruit_DHT not found. Please try : \n'
+          '1. Enter this at the command prompt to download the library:\n'
+          'git clone https://github.com/adafruit/Adafruit_Python_DHT.git\n'
+          '2. Change directories with:\n'
+          'cd Adafruit_Python_DHT\n'
+          '3. Now enter this:\n'
+          'sudo apt-get install build-essential python-dev\n'
+          '4. Then install the library with:\n'
+          'sudo python setup.py install\n')
+
+token = os.environ.get('MEMORY_TOKEN')
+url = os.environ.get('MEMORY_URL')
+headers = {'Authorization': 'Token {}'.format(token), 'Content-Type': 'application/json'}
+data = {"location": "beaubourg"}
+
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    print('Temp: {0:0.1f} C  Humidity: {1:0.1f}'.format(temperature, humidity))
+    try:
+        data.update({'type': 'temperature', 'data': temperature})
+        r = requests.post(url=url, data=json.dumps(data), headers=headers)
+        if r.status_code != 201 and r.status_code != 200:
+            raise Exception("HTTP {} for temperature".format(r.status_code))
+        print('Temperature added')
+        data.update({'type': 'humidity', 'data': humidity})
+        r = requests.post(url=url, data=json.dumps(data), headers=headers)
+        if r.status_code != 201 and r.status_code != 200:
+            raise Exception("HTTP {} for humidity".format(r.status_code))
+        print('Humidity added')
+        sleep(60)
+    except Exception as e:
+        print('{} : {}'.format(type(e).__name__, e))
+        sleep(300)
